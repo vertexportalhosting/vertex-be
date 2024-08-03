66a274d1c76cbbde86b2814db58ac31c45ef7a47
Vertex <vertexportalhosting@gmail.com>
2024-08-04 02:26:53 +0500

    update certificates

--- src/initialize-firebase.ts
+++ src/initialize-firebase.ts
@@ -3,18 +3,18 @@ import admin from 'firebase-admin';
 admin.initializeApp({
   credential: admin.credential.cert({
     // "type": "service_account",
-    "projectId": "kayrarr-14eac",
-    // "private_key_id": "c6445bfddc030aaa894284d2e2944d0a98c92e23",
-    "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCJoTiYkPGg9LB/\nHQv5woPeVNyhikj+tOq2ce1kctRkw1U4s04e0h0iZLZd77L/emigPrPPNHEUtFZO\nqqvbl02R3L+7EPDWsT7whkOFt8cohgJXvrMJy/r22SM8y06FaWTrSlvlIsBcHHug\nr2qZAPnk3/bjzeHee4WhknsKqWjyjwPPMDIhj+ty8RXdid5/yFPfRIMGnE5vuq8P\n/8cBo6XmkkikHv8aQlsDdgWva/YkWwCgjWM5ve3lWhB9mcw8kqK+gd1c9e+264NX\nZNtdEN0UuW4JopCq6jfKo8/DMRcD8h36oBc73sM/WvsrSJyHpAhMjpmp/a5X/poE\nrY/W5cYzAgMBAAECggEAFPXdxbaFZir92k4VLdOG+8xW0a9a0JfDC0gsjFjEqhfp\nmboNErqTP1KxR3si+q+s0YLbJZpqAmo4nsB1bfsMEEPbW2UIGNpjqIP6Mpo7lZtb\nLfvYDoznSsLlD/QyoDTd7sujVTFGtICN/3nXtTel10qYsjb/yD9tqZ7CaGxGlHIk\nRyuLMAx129z0LG8uqrtAXBR+Q05SwR+zDTGfN6bzWqSl4vJLfQCSZXZZldIYM6lK\nwE3bCued4H/m+LogjJE0mu2D8o/zrLTMTFxK61CFs16Wod5Vw7bOni8659GV0xYI\nMRQ7DAtD38bsm2V2zx1A9IIHbw8JOs27bFmKSjp2GQKBgQC+gYoEifZQ/q/ENXLf\n1f4LpAw97y5UilQOmsxaxLLOn9RuD6k2t7vnS1eYupxnCJqmOlLm1Lb+9daYdkuL\nWgCZ1z3PGWK+guiII0slLKM3Mf7we7dPcTsij5E0fgAW6spef+iPyg/EInZSYys6\nkF5sw3jwnbTJy6G5aT3D6E1VlQKBgQC48glTsOYQiFfJTEisfKaGhgp6ZQ2jUfXY\nRa6g9v4Mw60xuBTe5Zaz/nrWNt1OYJR1IrsoHnnlhQABzCGrO4bL1JCYQI0OWvW/\nFC2iMXf1WSQpxh29q/XDJ6+mwnkOjAOTfSsZWmivkzdKJ8HPukcRWKJynn8BEuQx\nu2Vd56aqpwKBgQCYeohkn6QmKeO3EOy0GcxvtssRm2oiwmELG31rPTq8xRK7UQ+o\nOyFLvHMNTHe4xBs75zzJgwJBSbWMbEqE6/HFMJFJPNFbGN5TcKRJydONbxfVlcAf\n9spVHEIlmKArt5toTcpvpFRAMTExOiy0gC8DsOUQ+bllNvZnt8N3FW0ojQKBgGwc\nYENh9zrW2Ko9Q4ZA339nv5xIki5UbOfd4/UILP39LLMRrnGVuADD076BF+EHKAFJ\nKiqLlioLEzP5xfy4sVVZjqTRpyMrHNhzzTEwxbTQRQl+XEhRyzonatCKggV052pC\nEv2hEe0Am6Nnli398Ck9rMhfylIwXuCLcPfxwEWlAoGBAJ4RNGs2/V4EiWTPQ1vA\n991v2pSzyphP7pUNjQG9U6RbX+/awhm9/yTocDYIStBLDozub0z9dh5q7a+26Zs3\nEtazLItSP0NPyJ72z54CbaboyfJWv4YUzGpO7c38UGBaGq++aDot/JsTRKujt7uQ\n+DEKncyEC124YORavSCWZ8PQ\n-----END PRIVATE KEY-----\n",
-    "clientEmail": "firebase-adminsdk-eed0s@kayrarr-14eac.iam.gserviceaccount.com",
-    // "client_id": "111961008317762285142",
+    // "project_id": "vertex-7543c",
+    // "private_key_id": "dfe4fd11154330bfbf05e8a7e8cceb59ffcca61e",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDIVrN4MrzI1nU6\nKqpk6pzY68UvusxlsDrLXVaDetIAU9Vb5N8qjdifcEDwI6Jss/b70c0jKHaae1q4\nHkofkbyCtfyOs3vNokyBDkAo/Ko7g6SFuswQsN3vi+rGFiTfpkHcnync5hxOtN2k\nES5l91GkWhg2NJ7EeMaoDtAKl3kQrvd3kMWYGTVYInvceUZLjjvBU5o6EqNUyp16\ntsH6EX968kXtKgz7/qgUK1x5N+rdGLlCKX4L4TPXa/RIIoZTEqIw6Ztrd9iD0S2s\nWibASzsfVaJhhQuwh72mxbVizIJiDAZPtICvUhLq9w9Qg7E/vReWGGqBD/YIJBpu\nJmuqB0N3AgMBAAECggEASt4X0L4JTfbTNUsXhYIl3mzxsl4ZH5Flz3UQrS77htLN\ng7t66NC8mQzev79ZZk006jOj+VUYSa/6nZ74LWcNrz1qZjvN8vGJrJsSQB74UShn\nlfBSbZRjRC+iK6e/3PKpX1T91IcMIh+2Sqd/Eu0Djit7K9qrUJogB1MNzT+BJ0nJ\nRNEurR5XQlPMczamThQqQs9SdNIN7rski1/t8SBhSNxrcZaoS1QzPiQ73e4ga99S\n6fUgCkg6LTfWwf/ybTGZLDvqj9yMxuKLCHGocFC/IHA29q28on8ICJlAOLvfSu9G\nmVuI89aoH/uor4KEyBf9C31Tz0YaZvmR9oVyZI3HfQKBgQD/ZuadppwICwuFBP15\nEPZSUBZrYg+mYSPGg3iSQfhSF/NtPWOOFbdaS2/yOx+osyst0IbuhuJbSrz31GDq\nSwUCpMca/59/eH3iEi6o2ghns8wI6C7d3k6TAr7UGH03tl0mxEb3VvtOUYAq5F1K\nIo7jtUM+uM0P4lPpoRK96dKb+wKBgQDIzsr42JCGqKC3Wjv0+6SVBapGKCbVKTMb\nBxrid/PbcOPddUlpHHhaGVPATr7umbXzFBrMBoAfyN0/547+V4uGuWYbI0mESY/j\n0tuL1/BkWUdIsoVPzi5/1qwIeNapwTmm8UAjOXSgT9FPj1YptX+NyAZXmTm68Ohc\n6P3SvhkBtQKBgQDyj6YCoBOcR/Ee2khpTMTEFC/WuIEogSVbCc3fCixuuzSD0UPV\n06YZaX/eTGya6d7eWidsLkqYbXje3qNUoP8XLauzZobyp1rW9HHAB6Ln6oijBPBN\n4y44zOizwrZZzSbXBJ5plgCCt0DEe7JmEEAo3yQIU7bdmRQc1M9Z8tIFNQKBgFM9\nndf/9hFMD6tR5W7NbdE0kvwYItjUBlJa5KMidFzv9BVWTEMdQSqgOx6Jxg4ke88y\n81TGERCb9FNMSvNWGQmVCyWOwCftUo9vVgEnV4hRlgIltqt5Xb8ynwe4B8xAxARX\nF25Cn/zgeXTQhzgs+54rX62qX8sFmF0DXEyi7rzNAoGBAPUPsCynowgtrzZ3r/jZ\noz58ueH7TxRRnLI3Z73yDdrMZ+szhy+5d203X4ElCt9hfVATchGz7zXJkrpJxesx\nUd/xEyVKr5l5eCHZFmEiAdfRKHIXR2RafxYP8Wa7pnCas3H0Rih6rcRVWqu9ky9h\nBnQZw2ZTVyh7foAOf1NY83AX\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-hslbm@vertex-7543c.iam.gserviceaccount.com",
+    // "client_id": "116876703057870441300",
     // "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     // "token_uri": "https://oauth2.googleapis.com/token",
     // "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    // "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eed0s%40kayrarr-14eac.iam.gserviceaccount.com"
-  }
-  ),
-  storageBucket: 'kayrarr-14eac.appspot.com',
+    // "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hslbm%40vertex-7543c.iam.gserviceaccount.com",
+    // "universe_domain": "googleapis.com"
+  }),
+  storageBucket: 'vertex-7543c.appspot.com',
 });
 
 const bucket = admin.storage().bucket();
